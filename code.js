c8f5ed0d8ae8f833f42a47624b9877fc10ce6ea6
fariveros <felixandresriveros@gmail.com>
2026-02-04 17:18:02 -0500

    Crear funcion cambiar color, y crear funcion para cambiar la opacidad del fondo

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ const container = document.getElementById('container')
 const button = document.getElementById('button')
 let cuadrosTotales = 16 * 16
 let prueba = 16
+let opacidad = 0
 container.classList.add("container")
 button.addEventListener("click", () => {
     let dimensionUsuario = parseInt(prompt("Ingrese el numero de cuadricula que desea"))
@@ -24,15 +25,24 @@ function generarCuadricula() {
         square.classList.add("square")
         container.appendChild(square)
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = 'blue'
+            square.style.backgroundColor = colorAleatorio()
         })
         square.addEventListener("mouseleave", () => {
             square.style.filter = "blur(2px)"
             square.style.contrast = "150%"
+            square.style.opacity = `${opacidad}`
+            opacidad = opacidad + 0.1
+            console.log(opacidad)
         })
         square.style.width = `calc(100%/${prueba})`
         square.style.height = `calc(100%/${prueba})`
     }
 }
-
+function colorAleatorio() {
+    let Red = Math.random() * 255
+    let Green = Math.random() * 255
+    let Blue = Math.random() * 255
+    let colores = `RGB(${Red},${Green},${Blue})`
+    return colores
+}
 generarCuadricula()
