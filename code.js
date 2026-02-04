1b6065c6a1e1e50c22181f9e5b82e7b01d476740
fariveros <felixandresriveros@gmail.com>
2026-02-04 16:47:32 -0500

    Poner el generador de cuadricula en una funcion, poner boton para generar cuadricula, crear funcion para el boton encargada de borrar la cuadricula existente y generar una con los datos del usuario siempre y cuando el valor ingresado sea menor a 100

--- code.js
+++ code.js
@@ -1,20 +1,38 @@
 const container = document.getElementById('container')
-container.classList.add("container")
-
+const button = document.getElementById('button')
+let cuadrosTotales = 16 * 16
 let prueba = 16
+container.classList.add("container")
+button.addEventListener("click", () => {
+    let dimensionUsuario = parseInt(prompt("Ingrese el numero de cuadricula que desea"))
+    if (dimensionUsuario >= 100) {
+        alert("Solo se permiten valores menores a cien (100).")
+    }
+    else if (dimensionUsuario < 100) {
+        cuadrosTotales = dimensionUsuario * dimensionUsuario
+        console.log(cuadrosTotales)
+        prueba = dimensionUsuario
+        container.innerHTML = ''
+        generarCuadricula()
+    }
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div")
-    square.classList.add("square")
-    container.appendChild(square)
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = 'blue'
-    })
-    square.addEventListener("mouseleave", () => {
-        square.style.filter = "blur(2px)"
-        square.style.contrast = "150%"
-    })
-    square.style.width = `calc(100%/${prueba})`
-    square.style.height = `calc(100%/${prueba})`
+})
 
+function generarCuadricula() {
+    for (let i = 0; i < cuadrosTotales; i++) {
+        const square = document.createElement("div")
+        square.classList.add("square")
+        container.appendChild(square)
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = 'blue'
+        })
+        square.addEventListener("mouseleave", () => {
+            square.style.filter = "blur(2px)"
+            square.style.contrast = "150%"
+        })
+        square.style.width = `calc(100%/${prueba})`
+        square.style.height = `calc(100%/${prueba})`
+    }
 }
+
+generarCuadricula()
